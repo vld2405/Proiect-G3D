f411bf6a7ba813e828158e13aead48ce09e4ddda
vld2405 <liviu.opris@student.unitbv.ro>
2024-12-01 21:46:37 +0200

    maybe woek

---
+++ Proiect_G3D/Proiect_G3D/Mesh.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <GLM.hpp>
+#include <gtc/matrix_transform.hpp>
+
+#include "Shader.h"
+#include "Texture.h"
+#include "Vertex.h"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Mesh
+{
+public:
+    // mesh Data
+    unsigned int numVertices;
+    std::shared_ptr <Vertex> vertices;
+    std::string name;
+
+    unsigned int numIndexes;
+    std::shared_ptr <unsigned int> indices;
+    vector<Texture>      textures;
+    unsigned int VAO;
+
+    Mesh(std::string name, const vector<Vertex>& vertices, const vector<unsigned int>& indices, const vector<Texture>& textures);
+    Mesh(std::string name, unsigned int numVertices, std::shared_ptr <Vertex> vertices, unsigned int numIndexes, std::shared_ptr <unsigned int> indices, const vector<Texture>& textures);
+    void Draw(Shader& shader);
+private:
+    // render data 
+    unsigned int VBO, EBO;
+    void setupMesh();
+};
+
