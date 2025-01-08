6c271009d92b6a737fdefbffd9ae0642c5d94d32
TudorPirău <tudorpirauioan@gmail.com>
2025-01-08 17:44:35 +0200

    Manually solved the conflicts
    Now the main.cpp should contain everything done until now.

--- Proiect_G3D/ViewOBJModel/main.cpp
+++ Proiect_G3D/ViewOBJModel/main.cpp
@@ -19,6 +19,7 @@
 #include <gtc/type_ptr.hpp>
 #include <glfw3.h>
 
+#include<map>
 #include <iostream>
 #include <fstream>
 #include <sstream>
@@ -35,6 +36,9 @@
 
 #include <mmsystem.h>
 #pragma comment(lib, "winmm.lib")
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
 
 // settings
 const unsigned int SCR_WIDTH = 1600;
@@ -92,6 +96,8 @@ std::vector<Model> trainStationObjModels;
 
 std::vector<std::string> trainStationNames = { "Brasov", "Predeal", "Sinaia", "Campina", "Ploiesti", "Bucuresti" };
 std::vector<std::pair<int, int>> activeStations;
+
+
 
 void SetVolume(float volume)
 {
@@ -379,9 +385,63 @@ void loadModels(std::string currentPath)
 		trainStationObjModels.push_back(Model(trainStationObjFileName, false));
 	}
 }
+struct Character {
+	GLuint TextureID;  // ID handle of the glyph texture
+	glm::ivec2 Size;   // Size of glyph
+	glm::ivec2 Bearing; // Offset from baseline to left/top of glyph
+	GLuint Advance;    // Offset to advance to next glyph
+};
+
+std::map<GLchar, Character> Characters;
+GLuint VAO, VBO;
+
+void RenderText(Shader& shader, std::string text, GLfloat x, GLfloat y, GLfloat scale, glm::vec3 color)
+{
+	// Activate corresponding render state
+	shader.use();
+	shader.SetVec3("textColor", color);
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(VAO);
+
+	// Iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++) {
+		Character ch = Characters[*c];
+
+		GLfloat xpos = x + ch.Bearing.x * scale;
+		GLfloat ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		GLfloat w = ch.Size.x * scale;
+		GLfloat h = ch.Size.y * scale;
+		// Update VBO for each character
+		GLfloat vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos,     ypos,       0.0, 1.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+
+			{ xpos,     ypos + h,   0.0, 0.0 },
+			{ xpos + w, ypos,       1.0, 1.0 },
+			{ xpos + w, ypos + h,   1.0, 0.0 }
+		};
+		// Render glyph texture over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// Update content of VBO memory
+		glBindBuffer(GL_ARRAY_BUFFER, VBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices); // Be sure to use glBufferSubData and not glBufferData
+
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// Render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// Now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.Advance >> 6) * scale; // Bitshift by 6 to get value in pixels (2^6 = 64)
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
 
 int main()
 {
+
 	// glfw: initialize and configure
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -428,6 +488,82 @@ int main()
 
 	loadModels(currentPath);
 	loadSounds(currentPath);
+
+	//freetype
+
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft)) {
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+		return -1;
+	}
+
+	FT_Face face;
+	if (FT_New_Face(ft, (currentPath + "\\Fonts\\chrusty-rock-font\\ChrustyRock-ORLA.ttf").c_str(), 0, &face)) {
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+		return -1;
+	}
+
+	FT_Set_Pixel_Sizes(face, 0, 48); // Set font size
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // Disable byte-alignment restriction
+
+	for (GLubyte c = 0; c < 128; c++) {
+		// Load character glyph
+		if (FT_Load_Char(face, c, FT_LOAD_RENDER)) {
+			std::cout << "ERROR::FREETYPE: Failed to load Glyph" << std::endl;
+			continue;
+		}
+		// Generate texture
+		GLuint texture;
+		glGenTextures(1, &texture);
+		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			face->glyph->bitmap.width,
+			face->glyph->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			face->glyph->bitmap.buffer
+		);
+		// Set texture options
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		// Store character for later use
+		Character character = {
+			texture,
+			glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+			glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+			face->glyph->advance.x
+		};
+		Characters.insert(std::pair<GLchar, Character>(c, character));
+	}
+	glBindTexture(GL_TEXTURE_2D, 0);
+
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(GLfloat), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	Shader textShader((currentPath + "\\Shaders\\text.vs").c_str(), (currentPath + "\\Shaders\\text.fs").c_str());
+	glm::mat4 projection = glm::ortho(0.0f, static_cast<GLfloat>(SCR_WIDTH), 0.0f, static_cast<GLfloat>(SCR_HEIGHT));
+	textShader.use();
+	textShader.setMat4("projection", projection);
+
+	//freetype
+
 
 	std::wstring idleMusicFilePathW = std::wstring(idleMusicFilePath.begin(), idleMusicFilePath.end());
 	PlaySound(idleMusicFilePathW.c_str(), NULL, SND_ASYNC | SND_LOOP);
@@ -572,6 +708,12 @@ int main()
 		{
 			lightingShader.SetVec3("lightColor", .1f, .1f, .1f);
 		}
+
+		float textPosX = 100.0f;
+		float textPosY = 1100.0f;
+
+		// Render text at the specified position
+		RenderText(textShader, "Hello, world!", textPosX, textPosY, 1.0f, glm::vec3(0.5, 0.8f, 0.2f));
 
 
 		//flyingCubeModel.Draw(lightingShader);
@@ -764,6 +906,8 @@ int main()
 	glfwTerminate();
 	return 0;
 }
+
+
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
